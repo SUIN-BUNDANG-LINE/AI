f7c9e09efda4f35ff3b1b1572dc45f99ec6bd931
Oh YoungJe <pine7420@naver.com>
2024-09-17 22:20:18 +0900

    [SBL-135] .env를 ignore에 추가

--- .github/workflows/prod_deploy.sh
+++ .github/workflows/prod_deploy.sh
@@ -3,7 +3,7 @@
 # sudo -u ubuntu sh <<EOF
 git checkout main
 git pull
-echo "$ENV_CONTENT" | base64 -d > .env
+echo "$ENV_CONTENT" | base64 -d > env
 docker build -t sulmoon2yong-ai:lastest .
 docker run -d --name sulmoon2yong-ai -p 8000:8000 sulmoon2yong-ai:lastest
 # EOF
