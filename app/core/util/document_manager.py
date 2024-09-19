56227985dd34866617d47820ea36621e303a409d
Oh YoungJe <pine7420@naver.com>
2024-09-19 12:57:09 +0900

    [SBL-135] 더이상 text_splitter를 사용하지 않음

--- app/core/util/document_manager.py
+++ app/core/util/document_manager.py
@@ -1,12 +1,11 @@
 import requests
 from langchain.docstore.document import Document
 from langchain_community.document_loaders import PyMuPDFLoader
-from app.core.config.text_spliiter import text_splitter
 
 class DocumentManager:
     def __init__(self):
         self.pdf_loader = PyMuPDFLoader
-
+    
     def documents_to_text(self, documents):
         documents_text = ""
         for document in documents:
@@ -14,6 +13,7 @@ class DocumentManager:
         ----------------------------
         {document.page_content}
         """
+            
         return documents_text
     
     def text_from_pdf_file_url(self, file_url: str):
@@ -24,6 +24,6 @@ class DocumentManager:
         response = requests.get(file_url)
         response.encoding = 'utf-8'
         text_content = response.text
-
+    
         documents = [Document(page_content=text_content)]
         return self.documents_to_text(documents)
