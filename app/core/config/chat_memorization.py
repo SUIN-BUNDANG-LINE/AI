8de1ab8c80fe6aa7a7570e453a7f5cad0b71d558
Oh YoungJe <pine7420@naver.com>
2024-09-30 13:00:56 +0900

    [SBL-151] redis url을 env에서 가져옴

--- app/core/config/chat_memorization.py
+++ app/core/config/chat_memorization.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
 from langchain_community.chat_message_histories import RedisChatMessageHistory
+
+load_dotenv()
+REDIS_URL = os.getenv("REDIS_URL")
 
 def get_message_storage(session_id: str):
     message_storage = RedisChatMessageHistory(
         session_id,
-        url="redis://localhost:6379/0"
+        url=REDIS_URL
     )
 
     return message_storage
