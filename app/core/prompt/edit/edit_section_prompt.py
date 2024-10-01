8038982dc2ce389ef808ddf00ecadaa9014a8a29
Oh YoungJe <pine7420@naver.com>
2024-10-01 16:20:01 +0900

    [SBL-151] 수정 프롬프트 수정

--- app/core/prompt/edit/edit_section_prompt.py
+++ app/core/prompt/edit/edit_section_prompt.py
@@ -4,8 +4,8 @@ edit_section_prompt = PromptTemplate(
     template="""
     ### Instructions
     1. Edit user survey data following prompt: {user_prompt}
-    2. Your modified the survey data must harmonize with prototype survey data
-    3. Find the section in the prototype survey data based on the title key of user survey data and reference it
+    2. Reference existing survey data
+    3. Find the section in existing survey data based on the title key of user survey data and reference it
 
     ### User Survey Data
     1. {user_survey_data}
--- app/core/prompt/edit/edit_total_survey_prompt.py
+++ app/core/prompt/edit/edit_total_survey_prompt.py
@@ -1,12 +1,10 @@
 from langchain.prompts import PromptTemplate
-from app.core.prompt.prompt_injection_block_prompt import prompt_injection_block_prompt
 
 edit_total_survey_prompt = PromptTemplate(
     template="""
     ### Instructions
     1. Edit user survey data following prompt: {user_prompt}
-    2. Your modified the survey data must harmonize with prototype survey data
-    3. Refence the Reference
+    2. Reference existing survey data
 
     ### User Survey Data
     1. {user_survey_data}
