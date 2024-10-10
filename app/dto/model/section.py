ce9dcd5c5375862fdfedbb56a53d4aff00e2ee42
Oh YoungJe <pine7420@naver.com>
2024-10-10 14:40:54 +0900

    [SBL-186] dto구조 변경 등

--- app/dto/model/section.py
+++ app/dto/model/section.py
@@ -1,8 +1,10 @@
+from typing import Optional
 from app.dto.model.question import Question
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, UUID4
 
 
 class Section(BaseModel):
+    id: Optional[UUID4] = Field(default=None, description="Unique identifier or null")
     title: str = Field(description="Title of the section")
     description: str = Field(description="Description of the section")
     questions: list[Question] = Field(description="Questions included in the section")
