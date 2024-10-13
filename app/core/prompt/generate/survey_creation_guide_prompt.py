dbcb3da54edad76cfbed36ec8641119d4fc161cb
Oh YoungJe <pine7420@naver.com>
2024-10-13 15:45:22 +0900

    [SBL-179] 질문 개수에 영향을 주는프롬프트 삭제

--- app/core/prompt/generate/survey_creation_guide_prompt.py
+++ app/core/prompt/generate/survey_creation_guide_prompt.py
@@ -5,16 +5,13 @@ survey_creation_guide_prompt = """
     2. **Place Personal Questions at the End**
     Structure the survey like a conversation. Start with simple, general questions and gradually move to more personal or demographic ones to keep respondents engaged.
 
-    3. **Keep the Survey Engaging and Comprehensive**
-    Structure the survey to respect respondents’ time by maintaining an appropriate number of questions while still allowing for sufficient feedback. Include a reasonable number(about 10) of questions to give participants the opportunity to reflect and provide thoughtful responses.
-
-    4. **Use Mostly Closed-Ended Questions**
+    3. **Use Mostly Closed-Ended Questions**
     Prefer multiple-choice or checkbox questions for easier responses and easier data analysis. Include only 1-2 open-ended questions at the end for additional insights.
 
-    5. **Avoid Leading Questions**
+    4. **Avoid Leading Questions**
     Ensure questions are neutral and do not bias responses. For example, ask “How helpful were our customer service representatives?” instead of “How helpful were our friendly customer service representatives?”
 
-    6. **Balance Response Options**
+    5. **Balance Response Options**
     Provide a symmetrical range of response choices to capture genuine feedback. For example:
     a. Very helpful  
     b. Helpful  
@@ -22,10 +19,10 @@ survey_creation_guide_prompt = """
     d. Unhelpful  
     e. Not helpful at all
 
-    7. **Use Precise Language**
+    6. **Use Precise Language**
     Avoid absolute terms like “always” or “never” that force binary answers. Instead of “Do you always eat breakfast?” consider “How often do you eat breakfast?”
 
-    8. **Avoid Double-Barreled Questions**
+    7. **Avoid Double-Barreled Questions**
     Ask about one topic at a time to prevent confusion. Instead of “Rate the quality of our products and support,” split into:
     - “Rate the quality of our products.”
     - “Rate the quality of our support.”
