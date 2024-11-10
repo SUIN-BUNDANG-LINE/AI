e0a25382b55346b044a96866f8f8033aac40d5bf
Oh YoungJe <pine7420@naver.com>
2024-11-10 15:19:12 +0900

    [SBL-230] 기본 구조 작성1

--- app/core/util/parallel_requestor.py
+++ app/core/util/parallel_requestor.py
@@ -1,21 +1,23 @@
 import asyncio
 from app.core.util.ai_manager import AIManager
+from app.core.util.allowed_other_manager import AllowedOtherManager
 
 # 초당 최대 20번의 요청을 허용하는 세마포어 생성
 semaphore = asyncio.Semaphore(20)
 
 
-async def limited_request(ai_manager: AIManager, user_prompt):
+async def limited_request(ai_manager: AIManager, survey_creation_prompt):
     # 세마포어가 허용될 때까지 대기
     async with semaphore:
         # 요청을 보낸 후 1초 대기
-        response = await ai_manager.async_chat(user_prompt)
+        response = await ai_manager.async_chat(survey_creation_prompt)
+        AllowedOtherManager.remove_last_choice_in_survey(parsed_survey)
         await asyncio.sleep(1)
 
         # 동기 파일 I/O로 결과를 기록
         with open("output_gpt_4o_mini.txt", "a") as f:
             f.write(f"{response}\n")
-    return response
+    return
 
 
 async def parallel_requestor(ai_manager: AIManager, user_prompt, loop_count):
--- app/service/survey_test_generate_service.py
+++ app/service/survey_test_generate_service.py
@@ -1,4 +1,3 @@
-import asyncio
 from langchain.output_parsers import PydanticOutputParser
 from app.core.prompt.document_summation_prompt import (
     document_summation_prompt,
@@ -6,16 +5,11 @@ from app.core.prompt.document_summation_prompt import (
 from app.core.util.ai_manager import AIManager
 from app.core.util.document_manager import DocumentManager
 from app.core.prompt.survey_creation_prompt import survey_creation_prompt
-from app.dto.response.survey_generate_response import SurveyGenerateResponse
 from app.dto.request.survey_generate_request import (
     SurveyGenerateRequest,
 )
 from app.core.util.function_execution_time_measurer import FunctionExecutionTimeMeasurer
 from app.dto.model.survey import Survey
-from app.core.util.allowed_other_manager import AllowedOtherManager
-from app.core.util.improve_user_prompt_with_search_chat import (
-    chat_improve_user_prompt_with_search,
-)
 from app.core.util.parallel_requestor import parallel_requestor
 
 
@@ -33,52 +27,23 @@ class SurveyTestGenerateService:
         chat_session_id = survey_generate_request.chat_session_id
         self.ai_manager = AIManager(chat_session_id)
 
-        user_prompt = FunctionExecutionTimeMeasurer.run_function(
-            "설문 비동기로 여러개 생성하는 태스크",
-            chat_improve_user_prompt_with_search,
-            self.ai_manager,
-            survey_generate_request.user_prompt,
-        )
-
         text_document = (
             self.document_manager.text_from_file_url(survey_generate_request.file_url)
             if survey_generate_request.file_url is not None
             else ""
         )
 
-        survey_generation_task = asyncio.create_task(
-            self.__generate_survey(
-                survey_generate_request.target,
-                survey_generate_request.group_name,
-                text_document,
-                user_prompt,
-            )
-        )
-
-        parsed_generated_survey = await survey_generation_task
-
-        return SurveyGenerateResponse(survey=parsed_generated_survey)
-
-    async def __generate_survey(
-        self,
-        target,
-        group_name,
-        text_document,
-        user_prompt,
-    ):
-        generated_survey = await FunctionExecutionTimeMeasurer.run_async_function(
-            "설문 생성 태스크",
-            self.ai_manager.async_chat,
-            self.survey_creation_prompt.format(
-                reference_materials=text_document + user_prompt,
-                user_prompt=user_prompt,
-                target=target,
-                group_name=group_name,
+        await FunctionExecutionTimeMeasurer.run_async_function(
+            "설문 비동기로 여러개 생성하는 태스크",
+            parallel_requestor,
+            self.ai_manager,
+            survey_creation_prompt.format(
+                reference_materials=text_document,
+                target=survey_generate_request.target,
+                group_name=survey_generate_request.group_name,
+                user_prompt=survey_generate_request.user_prompt,
             ),
-            self.parser_to_survey,
+            100,
         )
 
-        parsed_survey = self.parser_to_survey.parse(generated_survey)
-        print(parsed_survey.reason)
-
-        return AllowedOtherManager.remove_last_choice_in_survey(parsed_survey)
+        return
