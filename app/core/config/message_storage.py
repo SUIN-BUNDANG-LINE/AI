05bbee48a265644a666b73c30db574adea77b9cd
Oh YoungJe <pine7420@naver.com>
2024-12-18 15:53:19 +0900

    Upstash redis로 변경

--- app/core/config/message_storage.py
+++ app/core/config/message_storage.py
@@ -1,13 +1,20 @@
 import os
 from dotenv import load_dotenv
-from langchain_community.chat_message_histories import RedisChatMessageHistory
+from langchain_community.chat_message_histories import UpstashRedisChatMessageHistory
+
 
 load_dotenv()
-REDIS_URL = os.getenv("REDIS_URL")
-REDIS_TTL = os.getenv("REDIS_TTL")
+UPSTASH_REDIS_URL = os.getenv("UPSTASH_REDIS_URL")
+UPSTASH_REDIS_TOKEN = os.getenv("UPSTASH_REDIS_TOKEN")
+UPSTASH_REDIS_TTL = os.getenv("UPSTASH_REDIS_TTL")
 
 
 def get_message_storage(session_id: str):
-    message_storage = RedisChatMessageHistory(session_id, url=REDIS_URL, ttl=REDIS_TTL)
+    message_storage = UpstashRedisChatMessageHistory(
+        session_id=session_id,
+        url=UPSTASH_REDIS_URL,
+        token=UPSTASH_REDIS_TOKEN,
+        ttl=UPSTASH_REDIS_TTL,
+    )
 
     return message_storage
